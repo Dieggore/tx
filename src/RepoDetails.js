01e8fa8ffa77d636724fa9382dc6f7c0794a7aa0
dieggger <dieggger@gmail.com>
2018-09-13 15:58:06 -0600

    Corrected indentation in RepoDetails component

--- src/RepoDetails.js
+++ src/RepoDetails.js
@@ -4,40 +4,39 @@ export const RepoDetails = ({ repoDetails }) => {
   const { full_name, name, id, open_issues, description, html_url, contributors = [] } = repoDetails;
   return (
     <section className="repo-details-container">
-    <ol>
+      <ol>
       {Object.keys(repoDetails) ? (<h3>Project {name}</h3>) : (<h3>{'Click on any project in the list to load it\'s info'}</h3>)}
-      <div>
-        <p>Full name: {full_name}</p>
-        <p>Id: {id}</p>
-        <p>Issues: {open_issues}</p>
-        <p>
-          Description:<br/>
-          {description}
-        </p>
-        <p>
-          Details:
-          <br/>
-          <a href={html_url} target="_blank">Open details in new tab...</a>
-        </p>
-
         <div>
-          Contributors:
+          <p>Full name: {full_name}</p>
+          <p>Id: {id}</p>
+          <p>Issues: {open_issues}</p>
+          <p>
+            Description:<br/>
+            {description}
+          </p>
+          <p>
+            Details:
+            <br/>
+            <a href={html_url} target="_blank">Open details in new tab...</a>
+          </p>
           <div>
-            {contributors.map(c => (
-            <div key={c.id} className="contributor-container">
-              <img className="avatar" src={c.avatar_url} alt=""/>
-              <div>
-                <p>Login: {c.login}<br/></p>
-                <p>Contributions: {c.contributions}</p>
-                <a href={c.html_url} target="_blank">See contributor's github<br/></a>
+            Contributors:
+            <div>
+              {contributors.map(c => (
+              <div key={c.id} className="contributor-container">
+                <img className="avatar" src={c.avatar_url} alt=""/>
+                <div>
+                  <p>Login: {c.login}<br/></p>
+                  <p>Contributions: {c.contributions}</p>
+                  <a href={c.html_url} target="_blank">See contributor's github<br/></a>
+                </div>
               </div>
+              ))}
             </div>
-            ))}
+          </div>
         </div>
-        </div>
-      </div>
-    </ol>
-  </section>
+      </ol>
+    </section>
   )
 }
 
