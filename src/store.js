923613031145f2b325b8b2f92f3afd2f48c909cf
dieggger <dieggger@gmail.com>
2018-09-14 12:13:15 -0600

    Added semicolons

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import reducer from './reducers'
-import mySaga from './sagas'
+import reducer from "./reducers";
+import mySaga from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(mySaga);
 
