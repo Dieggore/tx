971491e7dfaf4615e83a32b3f4a496a876ab0501
dieggger <dieggger@gmail.com>
2018-09-14 12:24:17 -0600

    Code styling fixes

--- src/api.js
+++ src/api.js
@@ -1,19 +1,21 @@
 export const loadFacebookRepos = async () => {
- try {
-   const response = await fetch('https://api.github.com/users/facebook/repos');
-   const data = await response.json();
-   return data;
- } catch(e) {
-   console.log('e', e);
- }
-}
-
-export const loadRepoDetails = async (repoName) => {
   try {
-    const response = await fetch(`https://api.github.com/repos/facebook/${repoName}/contributors`);
+    const response = await fetch("https://api.github.com/users/facebook/repos");
     const data = await response.json();
     return data;
-  } catch(e) {
-    console.log('e', e);
+  } catch (e) {
+    console.log("e", e);
   }
-}
+};
+
+export const loadRepoDetails = async repoName => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/facebook/${repoName}/contributors`
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log("e", e);
+  }
+};
